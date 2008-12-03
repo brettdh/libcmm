bccca13b3b1793a1b2d58903302772f61cb93857
brettdh <brettdh@1971e5c1-a347-0410-b49b-82492f0cf60c>
2008-12-03 21:33:15 +0000

    added the timeops header for timing.
    git-svn-id: file:///home/svnshare/juggler_2.6.22.14/libcmm/trunk@177 1971e5c1-a347-0410-b49b-82492f0cf60c

---
+++ timeops.h
@@ -0,0 +1,60 @@
+#include <assert.h>
+#include <sys/time.h>
+#include <time.h>
+
+/*************************************** time operations */
+
+/* tvp should be of type TIMEVAL */
+#define TIME(tv)    (gettimeofday(&(tv),NULL))
+
+/* tvb, tve, tvr should be of type TIMEVAL */
+/* tve should be >= tvb. */
+#define TIMEDIFF(tvb,tve,tvr)                                    \
+do {                                                             \
+    assert(((tve).tv_sec > (tvb).tv_sec)                         \
+	   || (((tve).tv_sec == (tvb).tv_sec)                    \
+	       && ((tve).tv_usec > (tvb).tv_usec)));             \
+    if ((tve).tv_usec < (tvb).tv_usec) {                         \
+	(tvr).tv_usec = 1000000 + (tve).tv_usec - (tvb).tv_usec; \
+	(tvr).tv_sec = (tve).tv_sec - (tvb).tv_sec - 1;          \
+    } else {                                                     \
+	(tvr).tv_usec = (tve).tv_usec - (tvb).tv_usec;           \
+	(tvr).tv_sec = (tve).tv_sec - (tvb).tv_sec;              \
+    }                                                            \
+} while (0)
+
+
+/* Operations on timevals. */
+#define DEF_TIMEOPS
+#ifdef DEF_TIMEOPS
+#undef timerclear
+#undef timerisset
+#undef timercmp
+#undef timeradd
+#undef timersub
+
+#define timerclear(tvp)         (tvp)->tv_sec = (tvp)->tv_usec = 0
+#define timerisset(tvp)         ((tvp)->tv_sec || (tvp)->tv_usec)
+#define timercmp(tvp, uvp, cmp)                                         \
+        (((tvp)->tv_sec == (uvp)->tv_sec) ?                             \
+            ((tvp)->tv_usec cmp (uvp)->tv_usec) :                       \
+            ((tvp)->tv_sec cmp (uvp)->tv_sec))
+#define timeradd(tvp, uvp, vvp)                                         \
+        do {                                                            \
+                (vvp)->tv_sec = (tvp)->tv_sec + (uvp)->tv_sec;          \
+                (vvp)->tv_usec = (tvp)->tv_usec + (uvp)->tv_usec;       \
+                if ((vvp)->tv_usec >= 1000000) {                        \
+                        (vvp)->tv_sec++;                                \
+                        (vvp)->tv_usec -= 1000000;                      \
+                }                                                       \
+        } while (0)
+#define timersub(tvp, uvp, vvp)                                         \
+        do {                                                            \
+                (vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec;          \
+                (vvp)->tv_usec = (tvp)->tv_usec - (uvp)->tv_usec;       \
+                if ((vvp)->tv_usec < 0) {                               \
+                        (vvp)->tv_sec--;                                \
+                        (vvp)->tv_usec += 1000000;                      \
+                }                                                       \
+        } while (0)
+#endif /* DEF_TIMEOPS */
