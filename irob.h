cabd2285561a66983f23046de437f07b74cc93c4
brettdh <brettdh@1971e5c1-a347-0410-b49b-82492f0cf60c>
2009-06-19 19:23:33 +0000

    Re-commented to reflect reality.
    git-svn-id: file:///home/svnshare/juggler_2.6.22.14/libcmm/trunk@358 1971e5c1-a347-0410-b49b-82492f0cf60c

--- irob.h
+++ irob.h
@@ -15,6 +15,18 @@
  *     some amount of reordering for performance.
  */
 
+/* NOTE: though it does make sense to consider IROBs in the absence
+ * of multi-sockets, we are not currently implementing this.
+ * Functions below that would make up a non-multisocket IROB API have
+ * thus been commented out, with a NOTE explaining why.  The comments 
+ * on the non-multisocket IROB functions do apply to their multisocket 
+ * counterparts.
+ *
+ * Similarly, the safe-by-default "anonymous" IROB functions have 
+ * been made the default operations on multi-sockets, so they are no
+ * longer needed as separate functions.
+ */
+
 /* Identifer for an IROB, assigned by begin_irob[_label] 
  * and released by end_irob 
  */
@@ -24,13 +36,15 @@ typedef int irob_id_t;
  * If deps is non-NULL, it should point to a depnum-sized array of
  * irob_id_t's, indicating which IROBs this IROB depends on.
  * If deps is NULL, then this IROB has no dependencies. */
-irob_id_t begin_irob(int sock, const irob_id_t *deps, int depnum);
+//irob_id_t begin_irob(int sock, const irob_id_t *deps, int depnum);
+// NOTE: not implemented; non-multi-socket
 
-/* Begin an IROB on a multi-socket.
- * Such IROBs have labels and a thunk associated, just as
- * traditional multi-socket send operations do. */
+/* Begin an IROB on a multi-socket.  
+ * Such IROBs have sender labels, receiver labels, and a thunk
+ * associated, just as traditional multi-socket send operations do. */
 irob_id_t begin_irob_label(mc_socket_t sock, const irob_id_t *deps, int depnum,
-                           u_long labels, resume_handler_t fn, void *arg);
+                           u_long send_labels, u_long recv_labels, 
+                           resume_handler_t fn, void *arg);
 
 /* End an IROB, notifying the receiver that it is complete. */
 int end_irob(irob_id_t id);
@@ -43,22 +57,35 @@ ssize_t irob_send(irob_id_t irob_id, const void *buf, size_t len, int flags);
 int irob_writev(irob_id_t irob_id, const struct iovec *vector, int count);
 /* ...etc. */
 
+/* ---------------------------------------------------------------- * 
+ * NOTE: everything below this point is comments and unimplemented  *
+ * functions, as per the NOTE at the top of this header.            *
+ * ---------------------------------------------------------------- */
+
 /* These functions allow a single send operation inside a so-called
  * "anonymous" IROB that depends on all previous IROBs that have not
  * yet been received at the receiver application. 
  *
  * (This is the "default-safe" send that we talked about.) */
-ssize_t anon_irob_send(int sock, const void *buf, size_t len, int flags);
-int anon_irob_writev(int sock, const struct iovec *vector, int count);
+// ssize_t anon_irob_send(int sock, const void *buf, size_t len, int flags);
+// int anon_irob_writev(int sock, const struct iovec *vector, int count);
 /* ...etc. */
 
 /* Anonymous IROB sending operations on multi-sockets. */
+/* 
 ssize_t 
 anon_irob_mc_send(mc_socket_t sock, const void *buf, size_t len, int flags,
                   u_long labels, resume_handler_t fn, void *arg);
 int anon_irob_mc_writev(mc_socket_t sock, const struct iovec *vector, int count,
                         u_long labels, resume_handler_t fn, void *arg);
+*/
 /* ...etc. */
+/* NOTE: these are simply going to be the default cmm_send and cmm_writev;
+ * those functions are being replaced with functions that do the 
+ * safe-by-default anonymous IROB stuff described above.  Thus, it is
+ * easier to use the defaults safely than to misuse the irob_* functions
+ * and get unsafe reordering.
+ */
 
 
 /* and now for the receiver API */
@@ -67,15 +94,20 @@ int anon_irob_mc_writev(mc_socket_t sock, const struct iovec *vector, int count,
  * from a socket using these functions are guaranteed to be in an
  * order that respects the constraints specified by the sender's
  * IROBs. */
-ssize_t irob_recv(int sock, void *buf, size_t len, int flags);
-ssize_t irob_read(int sock, void *buf, size_t len);
+//ssize_t irob_recv(int sock, void *buf, size_t len, int flags);
+//ssize_t irob_read(int sock, void *buf, size_t len);
+// NOTE: not implemented; non-multi-socket
 /* ... */
 
 /* Receive an IROB's data bytes on a multi-socket.  If plabels is
  * non-NULL, the sender's labels will be stored at *plabels. */
+/*
 ssize_t irob_mc_recv(mc_socket_t sock, void *buf, size_t len, int flags,
-                     u_long *plabels /* OUT */);
+                     u_long *recv_labels);
 ssize_t irob_mc_read(mc_socket_t sock, void *buf, size_t len,
-                     u_long *plabels /* OUT */);
+                     u_long *recv_labels);
+*/
 /* ... */
-
+/* NOTE: since everything sent on a multi-socket is now an IROB, these
+ * are the default mc_recv and mc_read operations.
+ */
