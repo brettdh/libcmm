abf0bbea31253554f7a94365f827cf9e036afcfe
brettdh <brettdh@1971e5c1-a347-0410-b49b-82492f0cf60c>
2009-09-02 20:46:00 +0000

    now now, don't leave off the htonl.
    git-svn-id: file:///home/svnshare/juggler_2.6.22.14/libcmm/trunk@640 1971e5c1-a347-0410-b49b-82492f0cf60c

--- tests/end_to_end_tests_remote.cpp
+++ tests/end_to_end_tests_remote.cpp
@@ -52,6 +52,7 @@ EndToEndTestsRemote::testDefaultIROBOrdering()
             u_long label = ((i % 2 == 0) 
                             ? CMM_LABEL_ONDEMAND 
                             : CMM_LABEL_BACKGROUND);
+            nums[i] = htonl(nums[i]);
             int rc = cmm_send(send_sock, &nums[i], sizeof(nums[i]), 0,
                               label, NULL, NULL);
             CPPUNIT_ASSERT_EQUAL_MESSAGE("Sending integers", 
