a755a961ddbd39ec6331587e4af86a6afe22d94f
Brett <brettdh@ruff.(none)>
2009-10-02 15:22:23 -0400

    Trying the trickle test with shorter delays.

--- tests/trickle_tests.cpp
+++ tests/trickle_tests.cpp
@@ -103,13 +103,13 @@ TrickleTests::testTrickle()
         fg_args->sock = send_sock;
         fg_args->send_label = CMM_LABEL_ONDEMAND;
         fg_args->delay.tv_sec = 0;
-        fg_args->delay.tv_nsec = 700000000;
+        fg_args->delay.tv_nsec = 500000000;
 
         struct thread_args *bg_args = new struct thread_args;
         bg_args->sock = send_sock;
         bg_args->send_label = CMM_LABEL_BACKGROUND;
-        bg_args->delay.tv_sec = 2;
-        bg_args->delay.tv_nsec = 0;
+        bg_args->delay.tv_sec = 0;
+        bg_args->delay.tv_nsec = 600000000;
 
         for (size_t i = 0; i < NUMINTS; ++i) {
             fg_args->nums[i] = htonl(nums[i]);
