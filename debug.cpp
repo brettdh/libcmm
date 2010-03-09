ef6f8a7e9f7e83eabeebae3551b0a2e0c844622d
Brett Higgins <brettdh@eecs.umich.edu>
2010-03-09 08:13:36 -0500

    The dbgprintfs should no longer get split among multiple lines now.

--- debug.cpp
+++ debug.cpp
@@ -4,6 +4,11 @@
 #include "timeops.h"
 #include <pthread.h>
 #include "cmm_thread.h"
+#include <sstream>
+#include <string>
+#include <iomanip>
+using std::ostringstream; using std::string; 
+using std::setw; using std::setfill;
 
 #ifdef CMM_DEBUG
 static bool debugging = true;
@@ -15,20 +20,25 @@ void set_debugging(bool value)
 
 static void vdbgprintf(bool plain, const char *fmt, va_list ap)
 {
+    ostringstream stream;
     if (!plain) {
         struct timeval now;
         TIME(now);
-        fprintf(stderr, "[%lu.%06lu][%d][%s] ",
-                now.tv_sec, now.tv_usec, getpid(), 
+        stream << "[" << now.tv_sec << "." << setw(6) << setfill('0') << now.tv_usec << "]";
+        stream << "[" << getpid() << "]";
+        stream << "[";
 #ifdef CMM_UNIT_TESTING
-                "(unit testing)"
+        stream << "(unit testing)";
 #else
-                get_thread_name()
+        stream << get_thread_name();
 #endif
-                );
+        stream << "] ";
     }
+
+    string fmtstr(stream.str());
+    fmtstr += fmt;
     
-    vfprintf(stderr, fmt, ap);
+    vfprintf(stderr, fmtstr.c_str(), ap);
 }
 
 void dbgprintf(const char *fmt, ...)
