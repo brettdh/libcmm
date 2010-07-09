f0a5c443c88abc6433514476a926f09ba36f95e4
Brett Higgins <brettdh@eecs.umich.edu>
2010-07-09 08:58:49 -0400

    Changed the thunk check to use ipc_total_bytes_inflight.

--- csocket_sender.cpp
+++ csocket_sender.cpp
@@ -195,7 +195,8 @@ CSocketSender::Run()
             // 11ms so I can tell between the two.
             struct timespec rel_thunk_timeout = { 0, 11 * 1000 * 1000 };
             struct timespec thunk_timeout = abs_time(rel_thunk_timeout);
-            if (get_unsent_bytes(csock->osfd) > 0) {
+            //if (get_unsent_bytes(csock->osfd) > 0) {
+            if (ipc_total_bytes_inflight(csock->local_iface.ip_addr) > 0) {
                 timeout = thunk_timeout;
             }
 
