1b1bac8734502e5132e1209c1f695451dd78d7ee
Brett Higgins <brettdh@eecs.umich.edu>
2010-10-07 16:10:20 -0400

    Added new project for a really simple test app.  Two buttons: send fg and send bg. In progress. (Well, okay, also connect and disconnect.)

---
+++ AndroidSimpleSender/src/edu/umich/intnw/simplesender/SimpleSender.java
@@ -0,0 +1,15 @@
+package edu.umich.intnw.simplesender;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleSender extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
