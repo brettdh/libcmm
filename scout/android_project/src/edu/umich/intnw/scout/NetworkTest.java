8faf79e9939cc2fb099df4312dd239d7530c48b0
Brett Higgins <brettdh@eecs.umich.edu>
2011-04-29 11:12:55 -0400

    Updated to use external lib for network measurement.

--- scout/android_project/src/edu/umich/intnw/scout/NetworkTest.java
+++ scout/android_project/src/edu/umich/intnw/scout/NetworkTest.java
@@ -1,25 +1,16 @@
 package edu.umich.intnw.scout;
 
-import java.net.Socket;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
-import java.util.Date;
+
+import java.net.SocketException;
+
+import edu.umich.mobility.networktest.NativeNetworkTest;
 
 import android.util.Log;
 
 public class NetworkTest {
     private static String TAG = NetworkTest.class.getName();
     
-    final private static short BW_DOWN_PORT = 4321;
-    final private static short BW_UP_PORT = 4322;
-    final private static short RTT_PORT = 4323;
-
     final private static int timeoutSecs = 5;
-    final private static int connectTimeoutSecs = 10;
-    //final private static long timeoutNanosecs = timeoutSecs*1000*1000*1000;
     
     public String localAddr;
     public String remoteAddr;
@@ -27,9 +18,6 @@ public class NetworkTest {
     public int bw_up_Bps;
     public int rtt_ms;
 
-    final private static int CHUNKSIZE = 10000;
-    private byte[] bytes;
-    
     public NetworkTest(String local) {
         init(local, "141.212.110.132");
     }
@@ -41,37 +29,6 @@ public class NetworkTest {
     public void init(String local, String remote) {
         localAddr = local;
         remoteAddr = remote;
-        bytes = new byte[CHUNKSIZE];
-    }
-    
-    private void close_noexcept(Socket sock) {
-        try {
-            sock.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            // log, but ignore
-        }
-    }
-    
-    private Socket setupSocket(short port) throws IOException {
-        Log.d(TAG, "Connecting to " + remoteAddr + ":" + port);
-        Socket sock = new Socket();
-        sock.setSendBufferSize(1); // can't set it to zero; booo
-        sock.bind(new InetSocketAddress(InetAddress.getByName(localAddr), 0));
-        sock.setSoTimeout(timeoutSecs * 1000);
-        sock.connect(new InetSocketAddress(InetAddress.getByName(remoteAddr), port),
-                     connectTimeoutSecs * 1000);
-        return sock;
-    }
-    
-    private double secondsDiff(long startNanosecs, long endNanosecs) {
-        long diffNanosecs = endNanosecs - startNanosecs;
-        return ((double)diffNanosecs) / (1000000000.0);
-    }
-    
-    private double secondsDiff(Date start, Date end) {
-        long diff_ms = end.getTime() - start.getTime();
-        return ((double)diff_ms) / 1000.0;
     }
     
     public class NetworkTestException extends Exception {
@@ -80,147 +37,21 @@ public class NetworkTest {
         }
     };
 
-    private void testDownload() throws NetworkTestException {
-        Log.d(TAG, "Starting download test");
-        
-        Socket sock = null;
-        try {
-            sock = setupSocket(BW_DOWN_PORT);
-            //long startTime = System.nanoTime();
-            //long endTime = startTime + timeoutNanosecs;
-            Date startTime = new Date();
-            Date endTime = new Date(startTime.getTime() + timeoutSecs * 1000);
-            
-            int data = 0;
-            InputStream in = sock.getInputStream();
-            while (true) {
-                if ((new Date()).getTime() > endTime.getTime()) {
-                    break;
-                }
-                int rc;
-                rc = in.read(bytes);
-                if (rc <= 0) {
-                    throw new IOException();
-                }
-                data += rc;
-            }
-            
-            endTime = new Date();;
-            sock.close();
-            
-            bw_down_Bps = (int)((double)data/secondsDiff(startTime, endTime));
-        } catch (IOException e) {
-            e.printStackTrace();
-            if (sock != null) {
-                close_noexcept(sock);
-            }
-            throw new NetworkTestException("Download test failed: " + e.getMessage());
-        }
-    }
-    
-    private void testUpload() throws NetworkTestException {
-        Log.d(TAG, "Starting upload test");
-        
-        Socket sock = null;
-        try {
-            sock = setupSocket(BW_UP_PORT);
-            //long startTime = System.nanoTime();
-            //long endTime = startTime + timeoutNanosecs;
-            Date startTime = new Date();
-            Date endTime = new Date(startTime.getTime() + timeoutSecs * 1000);
-            
-            for (int i = 0; i < bytes.length; ++i) {
-                bytes[i] = '5';
-            }
-            int data = 0;
-            OutputStream out = sock.getOutputStream();
-            while (true) {
-                if ((new Date()).getTime() > endTime.getTime()) {
-                    break;
-                }
-                out.write(bytes);
-                data += bytes.length;
-            }
-            
-            endTime = new Date();
-            sock.close();
-            
-            bw_up_Bps = (int)((double)data / secondsDiff(startTime, endTime));
-        } catch (IOException e) {
-            e.printStackTrace();
-            if (sock != null) {
-                close_noexcept(sock);
-            }
-            throw new NetworkTestException("Upload test failed: " + e.getMessage());
-        }
-    }
-    
-    private void testRTT() throws NetworkTestException {
-        Log.d(TAG, "Starting RTT test");
-        
-        final int RTT_PACKET_SIZE = 64;
-        byte[] buf = new byte[RTT_PACKET_SIZE];
-        for (int i = 0; i < buf.length; ++i) {
-            buf[i] = '5';
-        }
-        
-        byte[] recvBuf = new byte[buf.length];
-
-        Date startTime = new Date();
-        Date lastResponseTime = null;
-        Date endTime = new Date(startTime.getTime() + timeoutSecs * 1000);
-        int tries = 0;
-        Socket sock = null;
-        try {
-            sock = setupSocket(RTT_PORT);
-            //long startTime = System.nanoTime();
-            //long endTime = startTime + timeoutNanosecs;
-            
-            InputStream in = sock.getInputStream();
-            OutputStream out = sock.getOutputStream();
-            while (true) {
-                if ((new Date()).getTime() > endTime.getTime()) {
-                    break;
-                }
-                
-                out.write(buf);
-                int rc = in.read(recvBuf);
-                if (rc <= 0) {
-                    throw new IOException();
-                }
-                tries++;
-                lastResponseTime = new Date();
-            }
-            
-            endTime = new Date();
-            sock.close();
-            
-            if (tries == 0) {
-                // shouldn't happen; socket op should have thrown
-                throw new NetworkTestException("RTT test failed; no responses");
-            }
-        } catch (IOException e) {
-            if (sock != null) {
-                close_noexcept(sock);
-            }
-            if (tries == 0) {
-                e.printStackTrace();
-                throw new NetworkTestException("RTT test failed: " + e.getMessage());
-            } else {
-                // check the RTT based on any responses we've gotten back
-                endTime = lastResponseTime;
-            }
-        }
-        rtt_ms = (int)(secondsDiff(startTime, endTime)*1000 
-                       / (double)tries);
-    }
-    
     public void runTests() throws NetworkTestException {
         Log.d(TAG, "Starting tests on " + localAddr);
-        testDownload();
-        testUpload();
-        testRTT();
-        Log.d(TAG, "Results: bw_down " + bw_down_Bps +
-              " bw_up " + bw_up_Bps + " rtt " + rtt_ms);
+        NativeNetworkTest nativeTest = new NativeNetworkTest(localAddr, remoteAddr);
+        
+        try {
+            Log.d(TAG, "Starting download test");
+            bw_down_Bps = nativeTest.runDownloadTest(timeoutSecs);
+            Log.d(TAG, "Starting upload test");
+            bw_up_Bps = nativeTest.runUploadTest(timeoutSecs);
+            Log.d(TAG, "Starting RTT test");
+            rtt_ms = nativeTest.runRTTTest();
+            Log.d(TAG, "Results: bw_down " + bw_down_Bps +
+                  " bw_up " + bw_up_Bps + " rtt " + rtt_ms);
+        } catch (SocketException e) {
+            throw new NetworkTestException(e.getMessage());
+        }
     }
 };
