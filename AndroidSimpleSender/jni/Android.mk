c4a1fcd44e82fde12fbe244ee058d7919ea80870
Brett Higgins <brett.higgins@gmail.com>
2011-07-02 15:55:26 -0400

    Small changes to AndroidSimpleSender to reflect new build process.

--- AndroidSimpleSender/jni/Android.mk
+++ AndroidSimpleSender/jni/Android.mk
@@ -2,6 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE:=cmm
+LOCAL_SRC_FILES:=../../libs/armeabi/libcmm.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
 common_C_INCLUDES := \
 	$(addprefix /Users/brettdh/src/android-source/,\
                 external/bdh_apps/libcmm)
@@ -13,8 +19,7 @@ LOCAL_SRC_FILES := ./intnw_ops.cpp
 
 LOCAL_C_INCLUDES := $(common_C_INCLUDES)
 LOCAL_CFLAGS := $(common_CFLAGS)
-LOCAL_LDLIBS := -L/Users/brettdh/src/android-source/out/target/product/generic/obj/lib \
-		-Wl,-rpath-link=/Users/brettdh/src/android-source/out/target/product/generic/obj/lib \
-		-lcmm -llog
+LOCAL_LDLIBS := -llog
+LOCAL_SHARED_LIBRARIES := cmm
 
 include $(BUILD_SHARED_LIBRARY)
