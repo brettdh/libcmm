b200a1eb47408e0daab74019558a6c00c57b8f7f
Brett Higgins <brettdh@eecs.umich.edu>
2011-07-07 13:41:28 -0400

    Some progress on the proxy thread.

--- tests/raw_socket_test.cpp
+++ tests/raw_socket_test.cpp
@@ -2,6 +2,10 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+/* Purpose: to figure out how to proxy a TCP connection
+ *          using raw sockets.
+ */
+
 const short LISTEN_PORT = 4210;
 
 typedef void * (*thread_func)(void*);
@@ -10,20 +14,48 @@ static pthread_mutex_t proxy_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t proxy_cv = PTHREAD_COND_INITIALIZER;
 static bool running = true;
 
+/* from_sock and to_sock are both raw sockets. */
+static int proxy_data(int from_sock, int to_sock)
+{
+    char buf[4096];
+    int rc = read(from_sock, buf, sizeof(buf));
+    if (rc > 0) {
+        int bytes_written = write(to_sock, buf, rc);
+        if (bytes_written != rc) {
+            perror("proxy_data: write");
+        }
+        rc = bytes_written;
+    } else if (rc == 0) {
+        fprintf(stderr, "proxy_data: socket closed\n");
+    } else {
+        perror("proxy_data: read");
+    }
+    return rc;
+}
+
 void ProxyThread(struct sockets *sockets)
 {
-    int proxy_sock = sockets->proxy_sock;
-    int server_sock = sockets->server_sock;
+    int client_proxy_sock = sockets->client_proxy_sock;
+    int server_proxy_sock = sockets->server_proxy_sock;
     
-    char buf[4096];
     fd_set readable;
     FD_ZERO(&readable);
-    FD_ZERO(&writeable);
-    FD_SET(proxy_sock, &readable);
-    FD_SET(proxy_sock, &writeable);
+    FD_SET(client_proxy_sock, &readable);
+    FD_SET(server_proxy_sock, &readable);
+    int nfds = max(proxy_sock, server_sock) + 1;
 
     while (running) {
-        int rc = select(/* ... */);
+        int rc = select(nfds, &readable, NULL, NULL, NULL);
+        if (rc > 0) {
+            if (FD_ISSET(client_proxy_sock, &readable)) {
+                proxy_data(client_proxy_sock, server_proxy_sock);
+                // TODO: error handling
+            }
+            if (FD_ISSET(server_proxy_sock, &readable)) {
+                proxy_data(server_proxy_sock, client_proxy_sock);
+                // TODO: error handling
+            }
+        }
     }
 }
 
