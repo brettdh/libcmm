97611d1658f1a8b8f73f6b7712bd9ab6738e73b9
Brett Higgins <brettdh@eecs.umich.edu>
2011-07-12 10:18:54 -0400

    Bug fix: the fd_set wasn't getting updated properly, causing no data to be proxied from the server.

--- tests/proxy_socket_test.cpp
+++ tests/proxy_socket_test.cpp
@@ -18,27 +18,29 @@ using std::max;
 
 const short LISTEN_PORT = 4210;
 
-typedef void * (*thread_func)(void*);
+typedef void * (*thread_func_t)(void*);
 
 static pthread_mutex_t proxy_started_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t proxy_started_cv = PTHREAD_COND_INITIALIZER;
 static bool running = true;
 
-/* from_sock and to_sock are both raw sockets. */
-static int proxy_data(int from_sock, int to_sock, void (*line_proc)(char *))
+static int proxy_data(int from_sock, int to_sock, bool (*line_proc)(int, char *))
 {
     char buf[4096];
     int rc = read(from_sock, buf, sizeof(buf) - 1);
     if (rc > 0) {
         buf[rc] = '\0';
+        bool send_onward = true;
         if (line_proc) {
-            line_proc(buf);
+            send_onward = line_proc(to_sock, buf);
         }
-        int bytes_written = write(to_sock, buf, rc);
-        if (bytes_written != rc) {
-            perror("proxy_data: write");
+        if (send_onward) {
+            int bytes_written = write(to_sock, buf, rc);
+            if (bytes_written != rc) {
+                perror("proxy_data: write");
+            }
+            rc = bytes_written;
         }
-        rc = bytes_written;
     } else if (rc == 0) {
         fprintf(stderr, "proxy_data: socket closed\n");
     } else {
@@ -48,42 +50,36 @@ static int proxy_data(int from_sock, int to_sock, void (*line_proc)(char *))
 }
 
 // only proxy a given number of lines before simulating disconnection.
+//  
 static void proxy_lines_until_closed(int client_fd, int server_fd, 
-                                     void (*line_proc)(char *),
-                                     int num_lines_before_break)
+                                     bool (*line_proc)(int, char *))
 {
-    fd_set readable;
-    FD_ZERO(&readable);
-    FD_SET(client_fd, &readable);
-    FD_SET(server_fd, &readable);
+    fd_set all_fds;
+    FD_ZERO(&all_fds);
+    FD_SET(client_fd, &all_fds);
+    FD_SET(server_fd, &all_fds);
     int nfds = max(client_fd, server_fd) + 1;
 
-    int num_lines = 0;
-
     while (running) {
+        fd_set readable;
+        FD_ZERO(&readable);
+        readable = all_fds;
         int rc = select(nfds, &readable, NULL, NULL, NULL);
-        if (num_lines_before_break >= 0 &&
-            num_lines >= num_lines_before_break) {
-            // silently drop it
-            continue;
-        }
         if (rc > 0) {
             if (FD_ISSET(client_fd, &readable)) {
                 rc = proxy_data(client_fd, server_fd, line_proc);
-                num_lines++;
-                if (rc > 0) {
-                    FD_SET(client_fd, &readable);
+                if (rc <= 0) {
+                    FD_CLR(client_fd, &all_fds);
                 }
             }
             if (FD_ISSET(server_fd, &readable)) {
                 rc = proxy_data(server_fd, client_fd, line_proc);
-                num_lines++;
-                if (rc > 0) {
-                    FD_SET(client_fd, &readable);
+                if (rc <= 0) {
+                    FD_CLR(server_fd, &all_fds);
                 }
             }
-            if (!FD_ISSET(client_fd, &readable) &&
-                !FD_ISSET(server_fd, &readable)) {
+            if (!FD_ISSET(client_fd, &all_fds) &&
+                !FD_ISSET(server_fd, &all_fds)) {
                 fprintf(stderr, "client and server proxy sockets closed; proxy thread exiting.\n");
                 break;
             }
@@ -94,14 +90,32 @@ static void proxy_lines_until_closed(int client_fd, int server_fd,
     }
 }
 
-static void print_line(char *line)
+static bool print_first_lines(int to_fd, char *line)
 {
-    fprintf(stderr, "proxy'd line: %s", line);
+    static int max_lines_proxied = 10;
+    static int lines_proxied = 0;
+
+    if (lines_proxied < max_lines_proxied) {
+        fprintf(stderr, "proxy'd line: %s", line);
+        ++lines_proxied;
+        return true;
+    } else {
+        fprintf(stderr, "suppressed line: %s", line);
+        return false;
+    }
 }
 
-void ProxyThread(void *unused)
+struct ports {
+    short proxy_port;
+    short server_port;
+};
+
+void ProxyThread(struct ports *ports)
 {
-    int client_proxy_listen_sock = make_listening_socket(LISTEN_PORT);
+    short proxy_port = ports->proxy_port;
+    short server_port = ports->server_port;
+
+    int client_proxy_listen_sock = make_listening_socket(proxy_port);
     assert(client_proxy_listen_sock >= 0);
 
     pthread_mutex_lock(&proxy_started_lock);
@@ -117,18 +131,29 @@ void ProxyThread(void *unused)
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(LISTEN_PORT + 1);
+    addr.sin_port = htons(server_port);
     socklen_t addrlen = sizeof(addr);
     int rc = connect(server_proxy_sock, (struct sockaddr *) &addr, addrlen);
     handle_error(rc < 0, "connecting server proxy socket");
 
-    proxy_lines_until_closed(client_proxy_sock, server_proxy_sock, print_line, 5);
+    proxy_lines_until_closed(client_proxy_sock, server_proxy_sock, print_first_lines);
 
     close(server_proxy_sock);
     close(client_proxy_sock);
     close(client_proxy_listen_sock);
 }
 
+
+int start_proxy_thread(pthread_t *proxy_thread, short proxy_port, short server_port)
+{
+    struct ports ports = {proxy_port, server_port};
+
+    pthread_mutex_lock(&proxy_started_lock);
+    int rc = pthread_create(proxy_thread, NULL, (thread_func_t) ProxyThread, &ports);
+    pthread_cond_wait(&proxy_started_cv, &proxy_started_lock);
+    pthread_mutex_unlock(&proxy_started_lock);
+    return rc;
+}
 
 void ServerThread(int listen_sock)
 {
@@ -150,18 +175,20 @@ void ServerThread(int listen_sock)
     close(sock);
 }
 
+bool suppress_downstream(int to_fd, char *line)
+{
+    return (to_fd != STDIN_FILENO);
+}
+
 int main()
 {
     int listen_sock = make_listening_socket(LISTEN_PORT + 1);
     assert(listen_sock >= 0);
     
     pthread_t server_thread, proxy_thread;
-    pthread_create(&server_thread, NULL, (thread_func) ServerThread, (void *) listen_sock);
+    pthread_create(&server_thread, NULL, (thread_func_t) ServerThread, (void *) listen_sock);
 
-    pthread_mutex_lock(&proxy_started_lock);
-    pthread_create(&proxy_thread, NULL, (thread_func) ProxyThread, NULL);
-    pthread_cond_wait(&proxy_started_cv, &proxy_started_lock);
-    pthread_mutex_unlock(&proxy_started_lock);
+    start_proxy_thread(&proxy_thread, LISTEN_PORT, LISTEN_PORT + 1);
 
     int client_sock = socket(PF_INET, SOCK_STREAM, 0);
     handle_error(client_sock < 0, "socket");
@@ -175,7 +202,7 @@ int main()
     int rc = connect(client_sock, (struct sockaddr *) &addr, (socklen_t) sizeof(addr));
     handle_error(rc < 0, "connecting client socket");
 
-    proxy_lines_until_closed(STDIN_FILENO, client_sock, NULL, -1);
+    proxy_lines_until_closed(STDIN_FILENO, client_sock, suppress_downstream);
     close(client_sock);
 
     pthread_join(proxy_thread, NULL);
@@ -183,3 +210,4 @@ int main()
 
     return 0;
 }
+
