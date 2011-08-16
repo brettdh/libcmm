d6ccc8473e8ae1a784c9678418b0463e0f3b4fe8
Brett Higgins <brettdh@eecs.umich.edu>
2011-08-16 16:52:12 -0400

    Small compile fix.

--- libcmm_external_ipc.h
+++ libcmm_external_ipc.h
@@ -1,6 +1,8 @@
 #ifndef LIBCMM_EXTERNAL_IPC_H_D69CUJHG
 #define LIBCMM_EXTERNAL_IPC_H_D69CUJHG
 
+#include <vector>
+
 int open_scout_socket();
 bool get_local_interfaces(std::vector<struct net_interface>& ifaces);
 
