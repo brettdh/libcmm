080411144a0cefd96c35710014e63b4d49b1b250
Brett Higgins <brett.higgins@gmail.com>
2011-12-03 00:41:12 -0500

    Added constructors to UndeliverableException.

--- javalib/src/edu/umich/intnw/UndeliverableException.java
+++ javalib/src/edu/umich/intnw/UndeliverableException.java
@@ -4,4 +4,11 @@ import java.io.IOException;
 
 public class UndeliverableException extends IOException {
     private static final long serialVersionUID = 1L;
+    public UndeliverableException() {
+        super();
+    }
+    
+    public UndeliverableException(String detailMessage) {
+        super(detailMessage);
+    }
 }
